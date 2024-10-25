07b0ba35246f00f32f5952312e3a0226f7314a5e
Zack Grannan <zgrannan@protonmail.com>
2024-10-24 21:38:28 -0700

    Fine-tune on directory

--- implementation/pipeline/main.py
+++ implementation/pipeline/main.py
@@ -1,3 +1,4 @@
+import PyPDF2
 import asyncio
 from openai import OpenAI, AsyncOpenAI
 import os
@@ -47,7 +48,9 @@ class QAPair(TypedDict):
     answer: str
 
 
-async def remove_duplicate_questions(questions: AsyncGenerator[str, None]) -> AsyncGenerator[str, None]:
+async def remove_duplicate_questions(
+    questions: AsyncGenerator[str, None]
+) -> AsyncGenerator[str, None]:
     seen = set()
     async for question in questions:
         if question not in seen:
@@ -55,10 +58,12 @@ async def remove_duplicate_questions(questions: AsyncGenerator[str, None]) -> As
             yield question
 
 
-T = TypeVar('T')
+T = TypeVar("T")
+
 
 async def slurp_generator(generator: AsyncGenerator[T, None]) -> List[T]:
     return [item async for item in generator]
+
 
 async def generate_qa_pairs_for_chunk(
     client: AsyncOpenAI, chunk: str, model: str, source: str, source_type: str
@@ -74,7 +79,9 @@ async def generate_qa_pairs_for_chunk(
     # in parallel
     questions_list = await slurp_generator(questions)
 
-    answers = await asyncio.gather(*[get_answer(client, chunk, question) for question in questions_list])
+    answers = await asyncio.gather(
+        *[get_answer(client, chunk, question) for question in questions_list]
+    )
 
     for question, answer in zip(questions_list, answers):
         yield {
@@ -88,17 +95,26 @@ class FinetuneEntry(TypedDict):
     answer: str
     source: str
 
+
 async def generate_qa_pairs(text: str, source: str, source_type: str) -> List[QAPair]:
     model = "meta-llama-3.1-8b-instruct-q6_k"
     client = get_async_client()
     chunks = split_text(text, chunk_size=5000, chunk_overlap=100)
     print(f"{len(chunks)} chunks created")
     chunked_results = await asyncio.gather(
-        *[slurp_generator(generate_qa_pairs_for_chunk(client, chunk, model, source, source_type)) for chunk in chunks]
+        *[
+            slurp_generator(
+                generate_qa_pairs_for_chunk(client, chunk, model, source, source_type)
+            )
+            for chunk in chunks
+        ]
     )
     return [qa_pair for qa_pair_list in chunked_results for qa_pair in qa_pair_list]
 
-async def generate_finetune_entries(filename: str, source: str, source_type: str) -> List[FinetuneEntry]:
+
+async def generate_finetune_entries(
+    filename: str, source: str, source_type: str
+) -> List[FinetuneEntry]:
     text = read_pdf(filename)
     qa_pairs = await generate_qa_pairs(text, source, source_type)
     return [
@@ -111,28 +127,60 @@ async def generate_finetune_entries(filename: str, source: str, source_type: str
     ]
 
 
+def extract_title(pdf_path):
+    with open(pdf_path, "rb") as file:
+        reader = PyPDF2.PdfReader(file)
+        # Attempt to extract title from metadata
+        if "/Title" in reader.metadata:
+            return reader.metadata["/Title"]
+        raise ValueError("No title found in metadata")
+
+
+async def generate_finetune_entries_from_file(filename: str) -> List[FinetuneEntry]:
+    source = extract_title(filename)
+    return await generate_finetune_entries(filename, source, "paper")
+
+
+async def generate_finetune_entries_for_files_in_directory(
+    directory: str,
+) -> List[FinetuneEntry]:
+    pdf_files = [
+        os.path.join(directory, filename)
+        for filename in os.listdir(directory)
+        if filename.endswith(".pdf")
+    ]
+
+    for file in pdf_files:
+        print(f"Processing file: {file}")
+
+    tasks = [generate_finetune_entries_from_file(file_path) for file_path in pdf_files]
+    results = await asyncio.gather(*tasks)
+
+    finetune_entries = [entry for result in results for entry in result]
+    return finetune_entries
+
+
 async def main():
     start_time = time.time()
 
     # Inputs
     ollama_base_url_embeddings = "http://localhost:11434/api/embeddings"
     embedding_model = "nomic-embed-text"
-    filename = "../data/Sohn_Visual_Prompt_Tuning_for_Generative_Transfer_Learning_CVPR_2023_paper.pdf"
-    source = "Visual Prompt Tuning for Generative Transfer Learning"
-    source_type = "paper"
 
     # Hugging Face
     repo_id = "CPSC532/arxiv_qa_data"
-    config_name = "test_dataset_2024OCT23"
+    config_name = "test_dataset_2024OCT24"
 
-    qa_pairs = await generate_finetune_entries(filename, source, source_type)
+    finetune_entries = await generate_finetune_entries_for_files_in_directory(
+        "../data"
+    )
 
     # Save to csv using Pandas
-    df = pd.DataFrame(qa_pairs)
+    df = pd.DataFrame(finetune_entries)
     df.to_csv(f"{config_name}_output.csv", index=False)
 
     # Upload to Huggingface
-    qa_pairs_dict = list_of_dicts_to_dict_of_lists(qa_pairs)
+    qa_pairs_dict = list_of_dicts_to_dict_of_lists(finetune_entries)
     upload_to_hf(
         data=qa_pairs_dict,
         repo_id=repo_id,
